0fb0106968e5c1b907232bc4900f630390d05445
HigeMan <kohei.otani@tutanota.com>
2023-06-16 16:56:14 +0900

    Initial Commit

---
+++ swiftuitest/swiftuitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitestApp.swift
+//  swiftuitest
+//
+//  Created by Kohei Otani on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
